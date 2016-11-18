970fc76980a146177e27fffdb2e60215ded365b7
Caio Alcantara <alcantaracaiolucas@gmail.com>
2016-11-18 13:33:21 -0300

    template literals and readm updated

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 ## Hi, here you'll find all exercises that i'm doing and have done since I started to study the new ES6 features.
 
+## <b>Chapter One:<b>
+### Let and Const
+* let and const are new variables declaration on ES6 that gives you power for create
+block scoped variables, and when you are using const, also forbids you from change them.
+
+### Template literals
+* Using the ` character to create a template string with easy replacement of content
+like:
+```JavaScript
+let languageName = "JavaScript";
+console.log(`${languageName} syntax highlighting`;
+```
+that corresponds to *JavaScript syntax highlighting* :)
+- - - -
--- es6-scripts/Template Strings/template_literals.js
+++ es6-scripts/Template Strings/template_literals.js
@@ -1,2 +1,35 @@
 // JS ES6 Study, this file contains the examples i've built when i'm studying th
 //e template literals and template strings
+
+// You can use double or single quotes to create strings
+let name = "Ryan";
+
+// Replacing the quotes for the ` (back) character, hey! Stills a string! :))
+let name = `Ryan`;
+let name = `Ryan
+with a new line without problems`;
+console.log(name);
+
+// Concat strings, are beautiful now!
+
+// Before ES6 template string
+let firstName = "Ryan";
+let lastName = 'Alfred';
+
+// So uggly :()
+console.log("My name is " + firstName + " " + lastName);
+
+// And now with ES6 Power!
+console.log(`My name is ${firstName} ${lastName}`);
+// Oh God, I'm crying!!! <3 <3
+
+// Now with some block of code <3
+let person = {
+    firstName: 'Ryan',
+    lastName: 'Alfred',
+    sayName() {
+        return `My name is ${this.firstName} ${this.lastName}`;
+    }
+}
+
+let name = person.sayName();
