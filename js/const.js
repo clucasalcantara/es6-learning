e8db47cbbc7f33aded4d650ee978a5192f6370a9
Caio Alcantara <caio@mw8solucoes.com.br>
2016-11-18 13:03:26 -0300

    Constants declarations studies

---
+++ js/const.js
@@ -0,0 +1,14 @@
+// JS ES6 Study, this file contains the examples i've built when i'm studying th
+//e const declaration of variables.
+
+
+//Var declaration of a constant
+var API_KEY = 'as45A$S$%AS$saihsoyW7y';
+API_KEY = 'hdiuags19td8%@$#f';
+// This results on a code break, because you're using uppercase on a variable,
+// that is a convention for constants, and you can change constants Dummy xD
+
+//ES6 Const declaration
+const API_KEY = 'as45A$S$%AS$saihsoyW7y';
+API_KEY = 'F**k the constant concept';
+//You can't change a constant :P Funny!
